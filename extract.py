2ea20f99f20c1c1293b9df86b0d23e54200e4ccf
MOON <133333114+M00N69@users.noreply.github.com>
2024-08-23 18:23:30 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -15,37 +15,84 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
-# Function to extract text and links from the website
-def extraire_texte_et_liens(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, 'html.parser')
+# URL of the GIF for the background
+gif_url = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzl1djM4anJ3dGQxY3cwYmM2M2VyeDI4cDUyM3ozcmNvNzJjOWg3aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26gJzajW8IiyJs3YY/giphy.gif"
 
-    table = soup.find('table')
-    if not table:
-        return None
+# Define the CSS for background and colors
+css_background = f"""
+<style>
+.stApp {{
+    background: url("{gif_url}") no-repeat center center fixed;
+    background-size: cover;
+    color: #F0F0F0;  /* Light text */
+}}
 
-    rows = table.find_all('tr')
-    data = []
+/* Apply background color to the entire sidebar */
+[data-testid="stSidebar"] > div:first-child {{
+    background-color: #037283 !important;  /* Blue-green */
+    color: #EDF6F9 !important;  /* Light text */
+}}
 
-    for row in rows:
-        cols = row.find_all('td')
-        if len(cols) < 7:  # Ensure that there are at least 7 columns to process
-            continue
+/* Style the inputs, selections, and buttons in the sidebar */
+.stSidebar input, .stSidebar selectbox, .stSidebar button {{
+    color: #EDF6F9 !important;  /* Light text */
+    background-color: #83c5be !important;  /* Light blue-green for buttons and inputs */
+}}
 
-        fiche = cols[0].text.strip()
-        resume_link = cols[1].find('a').get('href') if cols[1].find('a') else ''
-        resume = f"<a href='{resume_link}'>Résumé</a>"
-        publication = cols[2].text.strip()
-        date_publication = cols[3].text.strip()
-        titre = cols[4].text.strip()
-        rubrique_profil = cols[5].text.strip()
+/* Style the global buttons */
+button, .stButton > button {{
+    color: #fff !important; /* White text */
+    background-color: #3080F8 !important; /* Blue background */
+}}
 
-        # Append the extracted data as a list
-        row_data = [fiche, resume, publication, date_publication, titre, rubrique_profil]
-        data.append(row_data)
+button:hover, .stButton > button:hover {{
+    background-color: #1A5BB1 !important; /* Darker blue background */
+}}
 
-    return data
+/* Container for the table */
+.table-container {{
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}}
+
+/* Styles for the table */
+table {{
+    border-collapse: collapse;
+    width: 100%;  /* Ensure the table takes up the full width */
+    max-width: 100%;
+    border: 1px solid #ddd;
+    background-color: #29292F; /* Dark background */
+}}
+
+th, td {{
+    border: 1px solid #ddd;
+    text-align: left;
+    padding: 8px;
+    color: #F0F0F0;  /* Light text */
+    word-wrap: break-word;  /* Allow line breaks within cells */
+    white-space: normal;  /* Allow line breaks */
+}}
+
+tr:nth-child(even) {{
+    background-color: #333; /* Darker background for even rows */
+}}
+
+th {{
+    background-color: #333; /* Darker background for headers */
+    font-weight: bold;
+}}
+
+a {{
+    color: #00d9d9; /* Light blue for links */
+    text-decoration: none; /* Remove default underline */
+}}
+
+a:hover {{
+    text-decoration: underline; /* Underline on hover */
+}}
+</style>
+"""
 
 # Inject the CSS into the Streamlit app
 st.markdown(css_background, unsafe_allow_html=True)
@@ -190,15 +237,8 @@ def rasff_page():
 
                 st.subheader(f"Données RASFF pour {row[3]}")
 
-                # Configure AgGrid
-                gb = GridOptionsBuilder.from_dataframe(df_filtered)
-                gb.configure_pagination(paginationAutoPageSize=True)
-                gb.configure_side_bar()  # Add sidebar with filter options
-                gb.configure_default_column(editable=True, groupable=True, sortable=True, filter=True)
-                gridOptions = gb.build()
-
                 # Display interactive table
-                AgGrid(df_filtered, gridOptions=gridOptions, enable_enterprise_modules=True)
+                st.dataframe(df_filtered)
 
             except requests.exceptions.RequestException as e:
                 st.error(f"Erreur lors du téléchargement du fichier Excel: {e}")
@@ -218,3 +258,4 @@ if st.button("Editer"):
 # Sidebar button to display RASFF data page
 if st.sidebar.button("Afficher les données RASFF"):
     rasff_page()
+
